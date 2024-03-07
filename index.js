6d8a491f0afe600498112e08830a27026a07c0fb
junayet <muhammadjunayetmaruf@gmail.com>
2024-03-07 14:29:29 +0600

    blog notification has added

--- index.js
+++ index.js
@@ -18,8 +18,44 @@ const io = socketIo(server, {
   },
 });
 
+const users = {};
+
+const addUserSocket = (userId, socketId) => {
+  users[userId] = socketId;
+};
+
+const removeUserSocket = (userId) => {
+  delete users[userId];
+};
+
+const getUserSocket = (userId) => {
+  return users[userId];
+};
+
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log("A user connected", socket.id);
+
+  socket.on("user_connected", (data) => {
+    const { userId } = data
+    if (userId) {
+      addUserSocket(userId, socket.id);
+
+    }
+    console.log("Connected Users:", users);
+
+  });
+
+  socket.on("notifications", (notificationData) => {
+    const { receiverName } = notificationData;
+
+    receiverName.forEach((receiverNames) => {
+      const userSocketId = getUserSocket(receiverNames);
+      if (userSocketId) {
+        io.to(userSocketId).emit("notifications", notificationData);
+        console.log("receivers names are", userSocketId);
+      } else console.log("no receiver found");
+    });
+  });
 
   // Handle incoming messages
   socket.on("message", (message) => {
@@ -27,33 +63,36 @@ io.on("connection", (socket) => {
     // Broadcast the message to all connected clients
     io.emit("message", message);
   });
-  socket.on('refetch', (message) => {
-    console.log('Message received:', message);
+  socket.on("refetch", (message) => {
+    console.log("Message received:", message);
     // Broadcast the message to all connected clients
-    io.emit('refetch', message);
+    io.emit("refetch", message);
   });
   socket.on("unreadmessage", (message) => {
     console.log("Message received:", message);
     // Broadcast the message to all connected clients
     io.emit("unreadmessage", message);
   });
-  socket.on('unread_refetch', (message) => {
-    console.log('Message received:', message);
+  socket.on("unread_refetch", (message) => {
+    console.log("Message received:", message);
     // Broadcast the message to all connected clients
-    io.emit('unread_refetch', message);
+    io.emit("unread_refetch", message);
   });
   socket.on('read_unread_message', (message) => {
     console.log(message);
     io.emit('read_unread_message', message)
   })
-
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("User disconnected", socket.id);
+    // Find the user's name by their socket ID and remove them from the 'users' object
+    const userId = Object.keys(users).find((key) => users[key] === socket.id);
+    if (userId) {
+      removeUserSocket(userId);
+      console.log("removed suer is ", userId);
+    }
   });
 });
 // socketio connect  end
-
-
 
 server.listen(port, () => {
   console.log(`Fitness are Running on port ${port}`);
